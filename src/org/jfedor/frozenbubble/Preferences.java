d9940a276fede79d62355ab73e3a9e3958a58e2e
Walter Chacon <walter.chacon@gmail.com>
2013-12-03 02:04:44 -0500

    Preferences class is created containing the fields extracted from the FrozenBubble class

---
+++ src/org/jfedor/frozenbubble/Preferences.java
@@ -0,0 +1,15 @@
+package org.jfedor.frozenbubble;
+
+public class Preferences {
+	  private static boolean adsOn      = true;
+	  private static int     collision  = BubbleSprite.MIN_PIX;
+	  private static boolean compressor = false;
+	  private static int     difficulty = LevelManager.MODERATE;
+	  private static boolean dontRushMe = false;
+	  private static boolean fullscreen = true;
+	  private static int     gameMode   = FrozenBubble.GAME_NORMAL;
+	  private static boolean musicOn    = true;
+	  private static boolean soundOn    = true;
+	  private static int     targetMode = FrozenBubble.POINT_TO_SHOOT;
+
+}
